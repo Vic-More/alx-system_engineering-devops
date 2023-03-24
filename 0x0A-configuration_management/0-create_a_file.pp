c93668d4167f7ee46f7fec6739f833e0154fcef4
victor nwafor <victorokwy29@gmail.com>
2023-03-24 03:55:35 -0700

    all tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# This manifest creates a file at /tmp
-file { '/tmp/school':
-ensure  => 'file',
-owner   => 'www-data',
-group   => 'www-data',
-mode    => '0744',
-content => 'I love Puppet',
-}
+# Create file in /tmp
+#
+# file { '/tmp/school' :
+#     path    => '/tmp/school',
+#     mode    => '0744',
+#     owner   => 'www-data',
+#     group   => 'www-data',
+#     content => 'I love Puppet',
+#     }
